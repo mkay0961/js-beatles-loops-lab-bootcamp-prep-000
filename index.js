43f12bad472e460593b1d98eb9a896cc47c2c05b
Matthew Kay <mkay0961@gmail.com>
2019-10-19 20:17:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,24 +14,13 @@ for (var i =0; i < players.length; i++){
 return array;
 }
 
-function johnLennonFacts(){
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-var newfacts = [];
-
-var i = 0
-
-while (i<facts.length){
-  newfacts.push(facts[i] +"!!!");
-  i++;
-
-}
-console.log(newFacts)
-return newFacts
-
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
