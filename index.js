7fde1f43a9372740b9641eaf75bcd040329776ab
Matthew Kay <mkay0961@gmail.com>
2019-10-19 19:41:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments){
+var array = {}
 
+for (let i === 0; i < array.Length;i++){
+  if (i>=0){
+    array.push(`${musicians} plays ${instruments}`)
+  }
+}
+
+return array;
 }
