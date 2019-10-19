a4f56477da2ece90052ed28ec819d3bd3473d6cb
Matthew Kay <mkay0961@gmail.com>
2019-10-19 19:44:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 function theBeatlesPlay(musicians, instruments){
 var array = {}
 
-for (let i === 0; i < array.Length;i++){
+for (let i === 0; i < array.length;i++){
   if (i>=0){
     array.push(`${musicians} plays ${instruments}`)
   } else {
     "done"
   }
 }
-
+console.log
 return array;
 }
