a23db8e7cab8cbfda314365ff8b6f9f62c3031da
Matthew Kay <mkay0961@gmail.com>
2019-10-19 20:34:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,25 +25,15 @@ function johnLennonFacts() {
   return array;
 }
 
-// iLoveTheBeatles(num){
-//
-// var beatles = []
-// var num = 0
-//
-//   do {
-//   beatles.push("I love the beatles " + beatles[i] +"!" )
-//   num++
-// } while (num < 15)
-// console.log(beatles)
-// return beatles
-// }
+iLoveTheBeatles(num){
 
-function iLoveTheBeatles(num) {
-    var love = [];
-    do {
-        love.push("I love the Beatles!");
-        num++;
-    } while (num < 15);
+var beatles = []
+var num = 0
 
-    return love;
+  do {
+  beatles.push("I love the beatles!" )
+  num++
+} while (num < 15)
+console.log(beatles)
+return beatles
 }
