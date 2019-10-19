5b5ff3ce50953bfd9debb18ca8dd0a5a837ae7bc
Matthew Kay <mkay0961@gmail.com>
2019-10-19 19:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 // add solution here
 
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
 
 
 
 
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(players, instruments){
 var array = []
 
-for (var i === 0; i < musicians.length; i++){
-  if (i>=0){
+for (var i === 0; i < players.length; i++){
     array.push(musicians[i] +" plays "+ $instruments[i])
   }
 }
 
 return array;
 }
+
