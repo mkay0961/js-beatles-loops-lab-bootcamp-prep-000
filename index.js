199140f607a02f7feca3316d296cf87c75706060
Matthew Kay <mkay0961@gmail.com>
2019-10-19 19:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // add solution here
 
-function theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
-    }
-    return allPlayers;
-}
-
+// function theBeatlesPlay(players, instruments) {
+//     var allPlayers = [];
+//     for (var i=0; i < players.length; i++) {
+//         allPlayers.push(players[i] + " plays " + instruments[i]);
+//     }
+//     return allPlayers;
+// }
+//
 
 
 
