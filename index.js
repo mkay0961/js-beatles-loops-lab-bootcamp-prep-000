7cff761d05715f1f8aa7ab5be7109dba4eb7d75b
Matthew Kay <mkay0961@gmail.com>
2019-10-19 19:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments){
- players = []
- for (var i=0 ; players.Length; i++){
-   players.push(musicians[i] + "plays" + instruments[i])
- }
-   return players;
-}
-
-function johnLennonFacts(){
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-var newFacts = [];
-var i = 0;
-while (i < newFacts.Length) {
-  newFacts.push(facts[i] + "!!!")
-  i++;
-}
-return newFacts
+  
 }
