8d65a0ae0ddc36bb34f5a40ee46850d9300cb214
Matthew Kay <mkay0961@gmail.com>
2019-10-19 20:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,33 +4,9 @@
 
 
 function theBeatlesPlay(players, instruments){
-var array = []
-
-for (var i =0; i < players.length; i++){
-    array.push(players[i] + " plays "+ instruments[i])
-  }
-
-
-return array;
+var array = [];
+for (var i = 0; i<players.length; i++){
+  array.push(players[i] + " plays " + instruments[i])
 }
-
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
-  }
-  return array;
-}
-
-function iLoveTheBeatles(num){
-var beatles = []
-  do {
-  beatles.push("I love the Beatles!" )
-  num++
-} while (num < 15)
-console.log(beatles)
-return beatles
+return array
 }
